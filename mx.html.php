2d5813e831db908f993cdd0b2f2d917541635106
HarryMosdell <up712364@myport.ac.uk>
2024-04-07 19:59:27 +0100

    Add files via upload

---
+++ mx.html.php
@@ -0,0 +1,295 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+
+<link href="bb.css" rel="stylesheet">
+</head>
+
+<body class="font-mono vw-100 vh-100" >
+
+<?php  require 'nav.html.php'; ?>
+       
+    
+
+
+   <!--  header  starts here!-->
+    <div class="d-flex flex-grow-1 position-relative"  style=" flex-basis:0;  background-color: pink; ">
+
+        
+    <img src="https://spencercommunitytrust.co.uk/wp-content/uploads/2020/11/tempImageP0Sl80-scaled.jpg" style="z-index:1; color:black; width:100%;"> 
+    </img>
+<div class="position-absolute top-50 start-50 translate-middle" style="z-index:2; color:white;  ">
+     <h1 style="opacity:0.8;"> WELCOME TO BORUSSIA TEETH FC </h1>
+    </div>
+
+
+
+
+    </div>
+    
+  <!--  header  ends here!-->
+
+
+   <!-- introduction section starts here!-->
+
+    <div class="d-flex  px-5 py-4 justify-content-center" style=" border-bottom: solid 4px rgb(5, 63, 5); ">
+    
+        <div class="" style="max-width: 600px;">
+        
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+         printer took a galley of type and scrambled it to make a type specimen book. It has survived not 
+         only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+         It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+         and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+         </p>
+        </div>
+                </div>
+
+ <!-- introduction section ends here!-->
+
+
+
+  <!-- falicaties section  starts here!-->
+
+  <div class="" style=" border-bottom: solid 4px rgb(5, 63, 5); ">
+    
+     <h2 class="text-center"> Our falicities </h2>
+  <!-- container for falicaties section  starts here!-->
+    <div class="container-fluid ">
+
+
+    <!-- row for falicaties section  starts here!-->
+    <div class="row">
+    <div style="" class=" col-12 col-md-6 p-3 text-center columnHover">
+    <div style="" class=" scaleUp ">
+<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg3VpD9gM6kJW1b--ORJ3DXxHayt_4hx2UgHmGv6Dbvbm9NkXMxuQyrMxGcnHJFIxMg17UKtY6t2y-6bz4emdFf_eJHpM2KvTHCnEHxfi_aUKGte_McO3gpj8EGlLr2GK9Ri9sEFkI_lDQPJp23KzHgwZE3YKrDRpLh7zabxTuHLWfB5EZP-P4O3Pox/s4080/Milton%205.jpg"
+style="max-width:100%; max-height:600px;" class=""> </img>
+</div>
+</div>
+
+<div style="" class="col-12 col-md-6  p-md-5 text-center">
+<div style="">
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+         printer took a galley of type and scrambled it to make a type specimen book. It has survived not 
+         only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+         It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+         and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+         </p>
+</div>
+</div>
+
+ <!--  examaple column for falicaties section  starts here!-->
+<div class="col-12 col-md-6  p-3 text-center columnHover">
+<div class="bg-white d-flex flex-column align-items-center scaleUp h-100"> <!-- gutter for column  starts here!-->
+
+
+
+
+<div style="" class="" >  <!-- card header  starts here!-->
+
+ <h2>football falicaties </h2>
+
+</div>  <!-- card header  ends here!-->
+
+
+<div class="bg-info" style=" max-width:550px;">  <!-- card photo  starts here!-->
+
+ <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhEnIOHaNc-ncg-MdM4YQS-AbmH5zYkgOJAxvPIu32gzKqIaU6TbHWPfJOGAvfuWpryGoIwZN-07mWkriY7rVZOXN7D4wjlZNB4zChBpfphBMHvwfASP7rfBI-vl0vZGnnXmlQv94CK6M7QbxIQ5_tgqyC-PbmoOSjqkLvoqfbfvNN9KKJLV5MT9B0F/s600/Extras%202.JPG"
+  style="max-width:100%;"> </img>
+
+</div>  <!-- card photo  ends here!-->
+
+<div style="max-width:550px;"> <!-- card descrbition starts here!-->
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+         printer took a galley of type and scrambled it to make a type specimen book. It has survived not 
+       
+         </p>
+
+</div>   <!-- card descrbition ends here!-->
+
+</div>   <!-- gutter for column  ends here!-->
+
+
+</div>  <!--  examaple column for falicaties section  ends here!-->
+
+
+
+ <!--  examaple column for falicaties section  starts here!-->
+ <div class="col-12 col-md-6 bg-primary p-3 text-center columnHover">
+<div class="bg-white d-flex flex-column align-items-center scaleUp h-100"> <!-- gutter for column  starts here!-->
+
+
+
+
+<div style="font-style:bold;" class="" >  <!-- card header  starts here!-->
+
+<h2>drinking falicaties </h2>
+
+</div>  <!-- card header  ends here!-->
+
+
+<div class="bg-info" style=" max-width:550px;">  <!-- card photo  starts here!-->
+
+ <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjJQQxOuGLb7ODPJ4jNG-FuHwbQpa7CiJUv6GwBQzb9fmZ_OXIKZ7JWZpepyD0siViThBBH5haMSiBgd1PkjppRkItUhaEg8L65PGzbyQJFvjWvBGjNi3HcsoXM6M8JjXaovyVDXHurIYsAt7Kxt5Nu6RdG6G1suKsR7cZgclvjxOjl1gBpi1jFSi9-/s600/Milton%207.jpg"
+  style="max-width:100%;"> </img>
+
+</div>  <!-- card photo  ends here!-->
+
+<div style="max-width:550px;"> <!-- card descrbition starts here!-->
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+         printer took a galley of type and scrambled it to make a type specimen book. It has survived not 
+       
+         </p>
+
+</div>   <!-- card descrbition ends here!-->
+
+</div>   <!-- gutter for column  ends here!-->
+
+
+</div>  <!--  examaple column for falicaties section  ends here!-->
+
+
+</div>
+
+     <!-- row for falicaties section  starts here!-->
+
+    </div>
+      <!-- container for falicaties section  end here!-->
+
+            </div>
+
+<!-- falicaties section ends here!-->
+ 
+    <div class=" d-flex flex-column flex-lg-row " >
+      
+        <div class="p-3 d-flex flex-column justify-content-center align-items-center order-2 order-md-1" style="flex-grow:2; flex-basis:0;  ">
+            <div style="color: rgb(5, 63, 5);"> <h1> Club Location </h1> </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2445.7745738356703!2d-0.9235073234784342!3d52.19297017197737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48770f7f4a1bd649%3A0xed02f3e6f1fd9e08!2sMilton%20Football%20Club!5e0!3m2!1sen!2suk!4v1712342253121!5m2!1sen!2suk"
+            style="border:0;  width:100%; height:450px; " allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+     
+
+    
+        </div>
+       
+
+        <div class="d-flex justify-content-center flex-column align-items-center order-1 order-md-2" style="flex-grow:3; flex-basis:0; color: rgb(5, 63, 5); ">
+          
+           <div class=" px-md-5">
+
+
+                <a name='email'></a>
+                
+                  <!-- left side of the contact form section starts here !-->
+                  <h1 class="text-center" style=" font-size: 37px;"> CONTACT <span class="">  ME </span></h1>
+                
+                  <h3 class="d-none d-lg-block " style=" ">For more information about Boursia teeth</h3>
+                  <p class="d-block  d-lg-none mt-2" style="font-size:18px; "> For more information about Boursia teeth</p>
+                
+                </div>
+                 <!-- left side ends here !-->
+                
+                <!-- right section of contact form starts here !-->
+                <div class="  px-4  w-75 w-xs-100   mt-md-2 " >
+                  <form action="andreas.html.php" method="POST"  >
+               
+                
+                <div class=" d-flex flex-column flex-md-row justify-content-start gap-3 gap-md-5 mt-3 mt-md-3" >
+                
+                  <div class=" d-flex flex-column  justify-content-center  flex-grow-1" style="flex:1;">
+                
+                    <label class="text-start" for="fname">First name:</label>
+                    <input class=" px-2" style="height:60px; border: 3px solid rgb(5, 63, 5); border-radius: 25px; " 
+                    type="text" id="fname" name="fname" value="" >
+                   
+                    </div>
+                    <div class=" d-flex flex-column justify-content-center flex-grow-1" style="flex:1; ">
+                    <label class="text-start" for="lname">Last name:</label>
+                    <input class=" px-2" style="height:60px; border: 3px solid rgb(5, 63, 5); border-radius: 25px; " 
+                    type="text" id="lname" name="lname" value="" >              
+                </div>
+                
+                </div>
+                
+                <div class=" d-flex flex-column flex-md-row justify-content-center gap-3 gap-md-5 mt-3 mt-md-3" >
+                
+                  <div class=" d-flex flex-column  justify-content-center flex-grow-1" style="flex:1;">
+                
+                    <label class="text-start" for="Phone">Phone:</label>
+                    <input class=" px-2" style="height:60px; border: 3px solid rgb(5, 63, 5); border-radius: 25px; "  type="text" id="Phone" name="Phone" value="">
+            
+                    </div>
+                    <div class=" d-flex flex-column justify-content-center flex-grow-1" style="flex:1;">
+                    <label class="text-start" for="Email">Email:</label>
+                    <input class=" px-2" style="height:60px; border: 3px solid rgb(5, 63, 5); border-radius: 25px; "  type="text" id="Email" name="Email" value="">
+               
+                </div>
+                
+                </div>
+                
+                
+              
+                
+                
+                
+                
+                  <div class="d-flex flex-column mt-3 mt-md-4 mb-2 mb-md-4" style="">
+                
+                    <label class="text-start" for="desc">Description of enquiry:</label>
+                 
+                
+                    <textarea class="  px-2 " style=" height:200px; border: 3px solid rgb(5, 63, 5); border-radius: 25px;" id="desc" name="desc" value="">
+                  </textarea>
+                
+                    </div>
+                 
+                
+                
+                
+                <div class="Submita  d-flex   justify-content-end  mt-0 mt-md-0 mb-4" style="">
+                
+                   <input style="background-color: rgb(5, 63, 5);" class="btn flex-grow-1 flex-md-grow-0 w-50 change text-white rounded p-3 text-center" type="submit" id="submit1" name="submit1" value="Submit">
+                
+                   </div> 
+                
+                   </form>
+                
+                
+                
+                 
+                
+             
+                </div>
+
+
+
+            
+                
+                    </div>
+    
+ 
+ 
+                </div>
+     
+
+<footer class="py-5 text-white" style="background-color: rgb(5, 63, 5);">
+Production by Harrry Mosdell
+            </footer>
+
+
+  
+
+</body>
+
+</html>
